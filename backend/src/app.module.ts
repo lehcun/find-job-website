626572f84e53b015fd0dd5cfa5b22b1eb9ccca12
huy <thaihuy040@gmail.com>
2025-11-25 03:28:27 -0800

    them application api

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ApplicationModule } from './application/application.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ApplicationModule],
   controllers: [AppController],
   providers: [AppService],
 })
